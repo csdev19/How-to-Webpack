564f267d88215a0df132f972fea6c2b62d3074ff
Cristian Sotomayor <cristiansotomayor1912@gmail.com>
2018-10-17 14:37:17 -0500

    proyecto normal

--- ejemplo_entorno/webpack.config.js
+++ ejemplo_entorno/webpack.config.js
@@ -2,9 +2,8 @@ const path = require('path');
  
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'index.js'),
+    entry: './index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: './bundle.js'
     }
 }
